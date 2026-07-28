cfd88cecafada95a5e12330ee87d95df868a0bc0
petebanta <petebanta@gmail.com>
2026-07-28 13:11:57 -0400

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def main_app():
     def load_data():
         csv_file = 'style_finder_data.csv'
         if os.path.exists(csv_file):
-            df = pd.read_csv(csv_url, dtype=str)
+            df = pd.read_csv(csv_file, dtype=str)
             if 'Style_Number' in df.columns:
                 df['Style_Number'] = df['Style_Number'].astype(str)
             return df
